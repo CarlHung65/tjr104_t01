8feaea96f6e675e596b9ff1c1c96c128b6e300a7
Jessie_lin <lucky460721@gmail.com>
2026-02-28 11:23:26 +0800

    Pipeline_W moved from src/job_weather/dags to ./dags/ for better project structure.

--- dags/d_weatherapi_trace_historical_year.py
+++ dags/d_weatherapi_trace_historical_year.py
@@ -1,4 +1,4 @@
-from datetime import timedelta, datetime
+from datetime import timedelta, datetime, timezone
 from airflow.sdk import dag, task, TaskGroup
 import sys
 
@@ -7,12 +7,12 @@ if '/opt/airflow' not in sys.path:
     sys.path.append('/opt/airflow')
 
 # 2. 在sys.path之後才進行import
-from tasks.e_crawler_weather_gcp_refactor import e_get_uniq_acc_geo, prep_batch_plan, e_crawler_weatherapi
-from tasks.l_load_to_mysql_gcp import l_transform_and_load_to_mysql, l_summary_report
+from src.job_weather.OpenMeteo_crawler_weather.tasks.e_crawler_weather_gcp_refactor import e_get_uniq_acc_geo, prep_batch_plan, e_crawler_weatherapi
+from src.job_weather.OpenMeteo_crawler_weather.tasks.l_load_to_mysql_gcp import l_transform_and_load_to_mysql, l_summary_report
 
 # Default arguments for the DAG
 default_args = {
-    "owner": "tjr104-t01",  # DAG 擁有者名稱
+    "owner": "tjr104-t01-04-jessie",  # DAG 擁有者名稱
     "depends_on_past": False,  # 任務是否依賴前一次執行結果（False=獨立執行）
     "retries": 2,  # dag run失敗時最多重試2次，總計允許執行3次
     "retry_delay": timedelta(minutes=10),  # 除非task自己有額外定義，否則task重試需間隔10分鐘
@@ -24,7 +24,8 @@ default_args = {
     default_args=default_args,
     description="ETL process from requesting weather API for the weather data between 'January 01st~December 31th' until loading to MySQL database",
     schedule=None,
-    start_date=datetime(2026, 2, 16),
+    start_date=datetime(2026, 2, 25, 5, 00,
+                        tzinfo=timezone(offset=timedelta(hours=8))),
     catchup=False,
     tags=['traffic', 'weatherapi', 'taskflow'],
 )
@@ -34,16 +35,18 @@ def accident_weather_pipeline():
 
     for year in target_years:
         with TaskGroup(group_id=f"year_{year}") as year_group:
-            df = e_get_uniq_acc_geo(year, "test_accident")
+            df = e_get_uniq_acc_geo(year,
+                                    database="tjr104_t01")
             batches = prep_batch_plan(df, year, batch_size=50)
             # MappedOperator
             craw_done = e_crawler_weatherapi.partial(
                 target_year=year).expand(df_one_batch=batches)
 
-            report_done = l_summary_report(
-                target_year=year, upstream=craw_done)
-            load_done = l_transform_and_load_to_mysql(
-                target_year=year, database="test_weather", upstream=report_done)
+            report_done = l_summary_report(target_year=year,
+                                           upstream=craw_done)
+            load_done = l_transform_and_load_to_mysql(target_year=year,
+                                                      database="tjr104_t01",
+                                                      upstream=report_done)
 
         # 一年loading完之後換成下一年e_get_unqi_acc_geo執行
         if previous_year_group:
