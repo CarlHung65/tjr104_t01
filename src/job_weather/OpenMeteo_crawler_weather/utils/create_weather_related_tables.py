730ff204ad8542aaac9af620426b292872c45f25
Jessie_lin <lucky460721@gmail.com>
2026-03-06 23:41:16 +0800

    createTable_W corrected the typo.

--- src/job_weather/OpenMeteo_crawler_weather/utils/create_weather_related_tables.py
+++ src/job_weather/OpenMeteo_crawler_weather/utils/create_weather_related_tables.py
@@ -12,7 +12,7 @@ if '/opt/airflow' not in sys.path:
 from src.job_weather.OpenMeteo_crawler_weather.utils.create_engine_conn_tomysql import get_engine_sqlalchemy, get_conn_pymysql
 
 
-def create_weather_hist_table(table_name: str, *, database: str | None = None) -> pd.DataFrame:
+def create_weather_hist_table(table_name: str, *, database: str | None = None) -> None:
     """Create the table 'weather_data' into the assigned database
         in MySQL server (given by engine object) if the table is not exist.
     """
@@ -25,7 +25,7 @@ def create_weather_hist_table(table_name: str, *, database: str | None = None) -
 
     with engine.connect() as conn:
         ddl_text = text(f"""CREATE TABLE IF NOT EXISTS {table_name}(
-                                `id` BIGINT AUTO_INCREMENT,
+                                `weather_record_id` BIGINT AUTO_INCREMENT,
                                 `observation_datetime` DATETIME NOT NULL COMMENT '觀測日期時間(yyyy/mm/dd_HH:MM)',
                                 `temperature_degree` DECIMAL(6,2) COMMENT '氣溫(℃)',
                                 `apparent_temperature_degree` DECIMAL(6,2) COMMENT '體感溫度(℃)',
@@ -33,15 +33,17 @@ def create_weather_hist_table(table_name: str, *, database: str | None = None) -
                                 `precipitation_mm` DECIMAL(6,2) COMMENT '前一小時內降雨降雪量(mm)',
                                 `visibility_m` DECIMAL(6,2) COMMENT '能見度(m)',
                                 `weather_code` INT COMMENT '天氣代碼WMO code',
-                                `wind_speed_10m_km_per_h` DECIMAL(6,2) COMMENT '風速(km_per_hour)'
-                                `wind_gusts_10m_km_per_h` DECIMAL(6,2) COMMENT '最大陣風(km_per_hour)'
+                                `wind_speed_10m_km_per_h` DECIMAL(6,2) COMMENT '風速(km_per_hour)',
+                                `wind_gusts_10m_km_per_h` DECIMAL(6,2) COMMENT '最大陣風(km_per_hour)',
                                 `longitude_round` DECIMAL(6,2) COMMENT '經度(小數點後二位)',
                                 `latitude_round` DECIMAL(6,2) COMMENT '緯度(小數點後二位)',
                                 `created_on` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '建立日期',
                                 `created_by` VARCHAR(50) NOT NULL COMMENT '建立者',
                                 `hash_value` CHAR(32) NOT NULL,
                                 PRIMARY KEY (`id`),
-                                UNIQUE KEY UK_WHH_hash (`hash_value`))
+                                UNIQUE KEY UK_WHH_hash (`hash_value`),
+                                INDEX idx_{table_name}_long(`longitude_round`),
+                                INDEX idx_{table_name}_lat(`latitude_round`))
                                 CHARSET=utf8mb4 COMMENT '各地天氣觀測結果';""")
 
         conn.execute(ddl_text)
@@ -49,8 +51,10 @@ def create_weather_hist_table(table_name: str, *, database: str | None = None) -
         return None
 
 
-def create_view_table(ddl_str: str, *, database: str | None = None) -> None:
-    """create any view table as ddl descriptions."""
+def create_bridge_table(w_table_name: str, a_table_name: str,
+                        bridge_table_name: str,
+                        *, database: str | None = None) -> None:
+    """create any table for bridging as ddl descriptions."""
 
     # 準備與MySQL server的連線
     if database:
@@ -59,6 +63,20 @@ def create_view_table(ddl_str: str, *, database: str | None = None) -> None:
         engine = get_engine_sqlalchemy()
 
     with engine.connect() as conn:
+        ddl_str = f"""CREATE TABLE IF NOT EXISTS {bridge_table_name} (
+                        accident_id INT NOT NULL,
+                        weather_record_id INT NOT NULL,
+                        longitude_round DECIMAL(10, 2),
+                        latitude_round DECIMAL(10, 2),
+                        observation_datetime DATETIME,
+                        PRIMARY KEY (accident_id, weather_record_id),
+                        FOREIGN KEY (accident_id) REFERENCES {a_table_name}(accident_id),
+                        FOREIGN KEY (weather_record_id) REFERENCES {w_table_name}(weather_record_id)
+                        );
+
+                    CREATE INDEX idx_bridge_aID ON accident_weather_bridge(accident_id);
+                    CREATE INDEX idx_bridge_wID ON accident_weather_bridge(weather_record_id);
+                    """
         conn.execute(text(str(ddl_str)))
-        print("建立view表成功!")
+        print(f"建立{bridge_table_name}表成功!")
     return None
