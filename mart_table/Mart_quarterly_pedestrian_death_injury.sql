8a0efd95b748e7d7de0e76dd7c5529b31747dc1b
Jessie_lin <lucky460721@gmail.com>
2026-03-15 11:06:59 +0800

    mart_A fixed the logistic mistake on the duplicated death counts.

--- mart_table/Mart_quarterly_pedestrian_death_injury.sql
+++ mart_table/Mart_quarterly_pedestrian_death_injury.sql
@@ -12,7 +12,13 @@ CREATE OR REPLACE VIEW v_accident_sq1process_sq2process AS
 				UNION ALL
 				 SELECT	* FROM accident_new_sq2_process
 						WHERE vehicle_type_minor like "行人");
-					
+
+CREATE OR REPLACE VIEW v_accident_sq1process_sq2process_rn1 AS
+	(SELECT tmp.* FROM 
+		(SELECT *, ROW_NUMBER() OVER (PARTITION BY accident_id) AS rn
+					FROM v_accident_sq1process_sq2process) tmp
+			WHERE tmp.rn = 1);
+
 -- 2. 由於需要車禍日期、死傷人數，所以從sq1_main JOIN過來
 CREATE OR REPLACE VIEW v_accident_sq1main_newsq1main AS
 	(SELECT * FROM  accident_new_sq1_main
@@ -24,12 +30,12 @@ CREATE OR REPLACE VIEW v_accident_sq1prs_sq2prs_sq1m_newsq1m AS
             vm.accident_datetime, 
 			vm.death_count, 
             vm.injury_count
-		FROM v_accident_sq1process_sq2process vp
-			LEFT JOIN v_accident_sq1main_newsq1main vm
+		FROM v_accident_sq1process_sq2process_rn1 vp
+			INNER JOIN v_accident_sq1main_newsq1main vm
 				ON vp.accident_id = vm.accident_id);
                 
 -- 3. 建立Mart層圖表
-CREATE TABLE IF NOT EXISTS mart_quarterly_pedestrian_dj AS
+CREATE TABLE IF NOT EXISTS mart_monthly_pedestrian_dj AS
 	(SELECT
 		YEAR(accident_datetime) AS `year`,
 		QUARTER(accident_datetime) AS `quarter`,
@@ -41,7 +47,7 @@ CREATE TABLE IF NOT EXISTS mart_quarterly_pedestrian_dj AS
 					ORDER BY `year`, `quarter`);
 
 
-ALTER TABLE mart_quarterly_pedestrian_dj
+ALTER TABLE mart_monthly_pedestrian_dj
 	ADD COLUMN (
 				avg_monthly_death_btw_2021_2023 DECIMAL(10,1), 
 				stdev_monthly_death_btw_2021_2023 DECIMAL(10,1),
@@ -62,10 +68,10 @@ SELECT
     @`avg-sd_death` := AVG(death_monthly_total) - STDDEV(death_monthly_total),
     @`avg+sd_injury` := AVG(injury_monthly_total) + STDDEV(injury_monthly_total),
     @`avg-sd_injury` := AVG(injury_monthly_total) - STDDEV(injury_monthly_total)
-	FROM mart_quarterly_pedestrian_dj
+	FROM mart_monthly_pedestrian_dj
 	WHERE `year` BETWEEN 2021 AND 2023;
 
-UPDATE mart_quarterly_pedestrian_dj
+UPDATE mart_monthly_pedestrian_dj
 	SET 
 		avg_monthly_death_btw_2021_2023 = @avg_death,
 		stdev_monthly_death_btw_2021_2023 = @stdev_death,
@@ -76,6 +82,8 @@ UPDATE mart_quarterly_pedestrian_dj
 		`avg+stdev_monthly_injury_btw_2021_2023` = @`avg+sd_injury`,
 		`avg-stdev_monthly_injury_btw_2021_2023` = @`avg-sd_injury`;
 
-DROP VIEW v_accident_sq1process_sq2process;
+
+DROP VIEW  v_accident_sq1prs_sq2prs_sq1m_newsq1m;
 DROP VIEW v_accident_sq1main_newsq1main;
-DROP VIEW v_accident_sq1prs_sq2prs_sq1m_newsq1m;
+DROP VIEW v_accident_sq1process_sq2process_rn1;
+DROP VIEW v_accident_sq1process_sq2process;
